51b075da5e9fa6a7ae563368d3c2dd80639ea94e
LorrainelI <liyuanyulor@gmail.com>
2019-03-18 04:58:08 -0400

    image

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+
+var t = 0;
+var d = 0;
+var canvas;
+var scroll;
+
+var img;
+function setup() {
+  img= loadImage("logo.png");
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('sketch-holder');
+  //canvas.position(0,0);
+  // canvas.style('z-index', '-1');
+   //canvas.style('display', 'block');
+
+  fill(127,44,229);
+
+}
+
+function draw() {
+
+
+   d=map(mouseY,0,height,0,0.5);
+  background(193);
+image(img, width-100,30);
+
+  a= 1
+  b= 5
+
+  for (t = 0;t < 1600; t+=0.3) {
+
+    x = 680*sin(a*t+PI*d);
+		y = 350*sin(b*t)+50;
+		noStroke();
+    strokeWeight(0.5);
+
+
+		ellipse(width/2+x, height/2+y, 5, 5);
+	}
+
+}
